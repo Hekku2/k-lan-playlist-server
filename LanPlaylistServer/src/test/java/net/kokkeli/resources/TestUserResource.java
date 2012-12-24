f70aded5cc09cbb9de784275000179d0642ed417
Hekku2 <hekku2@gmail.com>
2012-12-25 01:18:10 +0200

    Added more tests for UserResource.

--- LanPlaylistServer/src/test/java/net/kokkeli/resources/TestUserResource.java
+++ LanPlaylistServer/src/test/java/net/kokkeli/resources/TestUserResource.java
@@ -1,11 +1,14 @@
 package net.kokkeli.resources;
 
+import javax.ws.rs.core.Response;
+
 import junit.framework.Assert;
 import net.kokkeli.data.ILogger;
 import net.kokkeli.data.Role;
 import net.kokkeli.data.User;
 import net.kokkeli.data.services.IUserService;
 import net.kokkeli.data.services.ServiceException;
+import net.kokkeli.resources.models.ModelUser;
 import net.kokkeli.resources.models.ViewModel;
 import net.kokkeli.server.ITemplateService;
 import net.kokkeli.server.RenderException;
@@ -18,7 +21,9 @@ import com.sun.jersey.api.NotFoundException;
 import static org.mockito.Mockito.*;
 
 public class TestUserResource {
-    private static long EXISTING_USER_ID;
+    private static long EXISTING_USER_ID = 54;
+    private static long NONEXISTING_ID = -3;
+    private static int RESPONSE_OK = 200;
     
     private ILogger mockLogger;
     private IUserService mockUserService;
@@ -28,15 +33,15 @@ public class TestUserResource {
     private UsersResource userResource;
     
     @Before
-    public void setup() throws NotFoundException, ServiceException, RenderException{
+    public void setup() throws NotFoundException, ServiceException {
         mockLogger = mock(ILogger.class);
         mockUserService = mock(IUserService.class);
         mockTemplateService = mock(ITemplateService.class);
         
-        existing = new User(EXISTING_USER_ID, "", Role.NONE);
+        existing = new User(EXISTING_USER_ID, "user", Role.NONE);
         
         when(mockUserService.get(EXISTING_USER_ID)).thenReturn(existing);
-        when(mockTemplateService.process(any(String.class), any(ViewModel.class))).thenReturn("");
+        when(mockUserService.get(NONEXISTING_ID)).thenThrow(new NotFoundException("User not found"));
         
         userResource = new UsersResource(mockLogger, mockTemplateService, mockUserService);
         
@@ -44,12 +49,8 @@ public class TestUserResource {
     
     @Test
     public void testGetDetailsThrowsNotFoundException() throws RenderException, ServiceException{
-        final long unexistingId = -3;
-
-        when(mockUserService.get(unexistingId)).thenThrow(new NotFoundException("User not found"));
-        
         try {
-            userResource.userDetails(null, unexistingId);
+            userResource.userDetails(null, NONEXISTING_ID);
             Assert.fail("Getting nonexisting user shoudl throw exception.");
         } catch (NotFoundException e) {
             // This should happen.
@@ -57,14 +58,56 @@ public class TestUserResource {
     }
     
     @Test
-    public void testServiceExceptionIsThrownWhenTemplateCantBeProcessed() throws RenderException{
-        when(mockTemplateService.process(any(String.class), any(ViewModel.class))).thenThrow(new RenderException("Rendergin failed"));
-        
+    public void testGetDetailsServiceExceptionIsThrownWhenTemplateCantBeProcessed() throws RenderException{
+        when(mockTemplateService.process(any(String.class), any(ViewModel.class))).thenThrow(new RenderException("Rendering failed"));
         try {
             userResource.userDetails(null, EXISTING_USER_ID);
             Assert.fail("Service exception should have been thrown.");
         } catch (ServiceException e) {
             // This should happen.
         }
+    }
+    
+    @Test
+    public void testGetDetailsPutsTemplateAndOkInResponse() throws NotFoundException, ServiceException, RenderException {
+        final String processedTemplate = "Jeeah";
+        
+        when(mockTemplateService.process(any(String.class), any(ViewModel.class))).thenReturn(processedTemplate);
+        
+        Response r = userResource.userDetails(null, EXISTING_USER_ID);
+        Assert.assertEquals(processedTemplate, r.getEntity().toString());
+        Assert.assertEquals(RESPONSE_OK, r.getStatus());
+        verify(mockTemplateService).process(anyString(), isA(ModelUser.class));
+    }
+    
+    @Test
+    public void testGetEditThrowsNotFoundException() throws ServiceException{
+        try {
+            userResource.userEdit(null, NONEXISTING_ID);
+            Assert.fail("Not found exception should have been thrown.");
+        } catch (NotFoundException e) {
+        }
+    }
+    
+    @Test
+    public void testGetEditThrowsServiceExceptionWhenTemplateServiceFails() throws RenderException{
+        when(mockTemplateService.process(any(String.class), any(ViewModel.class))).thenThrow(new RenderException("Rendering failed"));
+        try {
+            userResource.userEdit(null, EXISTING_USER_ID);
+            Assert.fail("Service exception should have been thrown.");
+        } catch (ServiceException e) {
+            // This should happen.
+        }
+    }
+    
+    @Test
+    public void testGetEditPutsTemplateAndOkInResponso() throws RenderException, NotFoundException, ServiceException{
+        final String processedTemplate = "Jeeah";
+        when(mockTemplateService.process(any(String.class), any(ViewModel.class))).thenReturn(processedTemplate);
+        
+        Response r = userResource.userEdit(null, EXISTING_USER_ID);
+        Assert.assertEquals(processedTemplate, r.getEntity().toString());
+        Assert.assertEquals(RESPONSE_OK, r.getStatus());
+        verify(mockTemplateService).process(anyString(), isA(ModelUser.class));
     }
 }
