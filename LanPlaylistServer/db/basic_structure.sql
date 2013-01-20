807026e1a302dda40e7aebf474fdba2bc0940646
Hekku2 <hekku2@gmail.com>
2013-01-20 20:23:35 +0200

    Added new playlist tables for future use.

--- LanPlaylistServer/db/basic_structure.sql
+++ LanPlaylistServer/db/basic_structure.sql
@@ -3,3 +3,22 @@ CREATE TABLE users(
     UserName varchar(255) UNIQUE,
     Role int 
 );
+
+CREATE TABLE tracks(
+    Id INTEGER NOT NULL PRIMARY KEY,
+    Track varchar(255),
+    Artist varchar(255),
+    Location varchar(255)
+);
+
+CREATE TABLE playlists(
+    Id INTEGER NOT NULL PRIMARY KEY,
+    Name varchar(255)
+);
+
+CREATE TABLE tracks_playlists(
+    TrackId INTEGER NOT NULL,
+    PlayListId INTEGER NOT NULL,
+    FOREIGN KEY(TrackId) REFERENCES tracks(Id),
+    FOREIGN KEY(PlayListId) REFERENCES playlists(Id)
+);
--- LanPlaylistServer/db/test_data.sql
+++ LanPlaylistServer/db/test_data.sql
@@ -1,2 +1,28 @@
 INSERT INTO users (UserName, Role) VALUES ('admin',2);
 INSERT INTO users (UserName, Role) VALUES ('user',1);
+
+-- Tracks
+INSERT INTO tracks (Track, Artist, Location) VALUES ('Seitan is guud', 'Lucifer Virtanen', 'D:\Music\song.ogg');
+INSERT INTO tracks (Track, Artist, Location) VALUES ('March enemy hell matti', 'Lucifer Virtanen', 'D:\Music\song3.ogg');
+INSERT INTO tracks (Track, Artist, Location) VALUES ('Jarmonet', 'Raimo Belsebup', 'D:\Music\song2.ogg');
+
+-- Playlists
+INSERT INTO playlists (Name) VALUES ('Joululan 2014');
+INSERT INTO playlists (Name) VALUES ('Empty list');
+
+-- Tracks to playlists
+INSERT INTO tracks_playlists (TrackId, PlayListId)
+VALUES (
+    (SELECT Id FROM tracks WHERE Track like 'Seitan is guud' and Artist like 'Lucifer Virtanen'),
+    (SELECT Id FROM playlists WHERE Name like 'Joululan 2014')
+);
+INSERT INTO tracks_playlists (TrackId, PlayListId)
+VALUES (
+    (SELECT Id FROM tracks WHERE Track like 'March enemy hell matti' and Artist like 'Lucifer Virtanen'),
+    (SELECT Id FROM playlists WHERE Name like 'Joululan 2014')
+);
+INSERT INTO tracks_playlists (TrackId, PlayListId)
+VALUES (
+    (SELECT Id FROM tracks WHERE Track like 'Jarmonet' and Artist like 'Raimo Belsebup'),
+    (SELECT Id FROM playlists WHERE Name like 'Joululan 2014')
+);
