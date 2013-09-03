6670e0cfb5916faf0d659c43094808572243ee93
Hekku2 <hekku2@gmail.com>
2013-09-03 20:59:38 +0300

    Fixed same bug from other lists.

--- LanPlaylistServer/src/main/resources/net/kokkeli/resources/js/playlist.js
+++ LanPlaylistServer/src/main/resources/net/kokkeli/resources/js/playlist.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$("a[class*='delete']").click(function (event) {
-		var element = $(event.target);
+		var element = $(event.currentTarget);
 		$.post(element.attr("href"), { "id": element.attr('id') });
 		return false;
 	});
--- LanPlaylistServer/src/main/resources/net/kokkeli/resources/js/tracks.js
+++ LanPlaylistServer/src/main/resources/net/kokkeli/resources/js/tracks.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$("button[class*='icon-plus-sign']").click(function (event) {
-		$.post('/addToQueue', { "id": $(event.target).attr('id') });
+		$.post('/addToQueue', { "id": $(event.currentTarget).attr('id') });
 		
 	});
 });
