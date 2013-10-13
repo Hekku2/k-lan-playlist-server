39fca78d9309e629029fa6494a7679140fdeeecb
Hekku2 <hekku2@gmail.com>
2013-10-13 21:27:47 +0300

    Now track in fetch request is also added to playlist.

--- LanPlaylistServer/src/main/java/net/kokkeli/data/services/FetchRequestService.java
+++ LanPlaylistServer/src/main/java/net/kokkeli/data/services/FetchRequestService.java
@@ -8,10 +8,13 @@ import com.google.inject.Inject;
 import net.kokkeli.data.FetchRequest;
 import net.kokkeli.data.ILogger;
 import net.kokkeli.data.LogSeverity;
+import net.kokkeli.data.PlayList;
 import net.kokkeli.data.Track;
 import net.kokkeli.data.db.DatabaseException;
 import net.kokkeli.data.db.IFetchRequestDatabase;
+import net.kokkeli.data.db.IPlaylistDatabase;
 import net.kokkeli.data.db.ITrackDatabase;
+import net.kokkeli.data.db.NotFoundInDatabase;
 
 /**
  * Service for fetch requets
@@ -22,17 +25,21 @@ public class FetchRequestService implements IFetchRequestService {
     private final ILogger logger;
     private final IFetchRequestDatabase fetchRequestDatabase;
     private final ITrackDatabase tracks;
+    private final IPlaylistDatabase playlists;
     
     /**
      * Creates fetch request service
      * @param logger Logger
      * @param fetchRequestDatabase Database containing fetch requests
+     * @param tracks Tracks database
+     * @param playlists Playlists database
      */
     @Inject
-    public FetchRequestService(ILogger logger, IFetchRequestDatabase fetchRequestDatabase, ITrackDatabase tracks){
+    public FetchRequestService(ILogger logger, IFetchRequestDatabase fetchRequestDatabase, ITrackDatabase tracks, IPlaylistDatabase playlists){
         this.logger = logger;
         this.fetchRequestDatabase = fetchRequestDatabase;
         this.tracks = tracks;
+        this.playlists = playlists;
     }
     
     @Override
@@ -59,11 +66,22 @@ public class FetchRequestService implements IFetchRequestService {
                track = tracks.add(track);
             }
             
+            //If playlist is set, add this track to it.
+            if (request.getPlaylist() != null){
+                PlayList playlist = playlists.get(request.getPlaylist().getId());;
+                playlist.getItems().add(track);
+                
+                playlists.update(playlist);
+            }
+
             request.setLastUpdated(new Date());
             fetchRequestDatabase.add(request);
         } catch (DatabaseException e) {
             logger.log("Something went wrong with the database while adding fetch request.", LogSeverity.ERROR);
             throw new ServiceException("Something went wrong with the database while adding fetch request.", e);
+        } catch (NotFoundInDatabase e) {
+            logger.log("Trying to create fetch request with a playlist that doesn't exist.", LogSeverity.ERROR);
+            throw new IllegalArgumentException("Playlist did not exist");
         }
     }
 
