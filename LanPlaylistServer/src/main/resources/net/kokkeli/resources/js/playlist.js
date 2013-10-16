cf04419b3fbc0a16a4dc471e24cf3d36dcafc052
Hekku2 <hekku2@gmail.com>
2013-10-16 17:33:26 +0300

    Playlist view is now refreshed when track is removed by user.

--- LanPlaylistServer/src/main/resources/net/kokkeli/resources/js/playlist.js
+++ LanPlaylistServer/src/main/resources/net/kokkeli/resources/js/playlist.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	$("a[class*='delete']").click(function (event) {
 		var element = $(event.currentTarget);
-		$.post(element.attr("href"), { "id": element.attr('id') });
+		$.post(element.attr("href"), { "id": element.attr('id') }, function() {
+			document.location.reload(true);
+		});
 		return false;
 	});
 });
