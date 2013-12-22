e2839c6bcd6b097236b929b4351e591ebc131ae6
Hekku2 <hekku2@gmail.com>
2013-12-22 16:59:23 +0200

    Now log lines are actually taken from database.

--- LanPlaylistServer/src/main/java/net/kokkeli/data/db/LogTable.java
+++ LanPlaylistServer/src/main/java/net/kokkeli/data/db/LogTable.java
@@ -1,11 +1,10 @@
 package net.kokkeli.data.db;
 
 import java.io.File;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
-
 import com.almworks.sqlite4java.SQLiteConnection;
 import com.almworks.sqlite4java.SQLiteException;
 import com.almworks.sqlite4java.SQLiteStatement;
@@ -22,12 +21,24 @@ public class LogTable {
     private static final String DATEFORMAT = "yyyy-MM-dd hh:mm:ss";
     private static final SimpleDateFormat formatter = new SimpleDateFormat(DATEFORMAT);
     
+    private static final String GETALL = "SELECT Timestamp, Severity, Message, Source FROM " + TABLENAME;
+    
     private final String databaseLocation;
     
+    /**
+     * Creates a new instance of log table
+     * @param databaseLocation
+     */
     public LogTable(String databaseLocation) {
         this.databaseLocation = databaseLocation;
     }
     
+    /**
+     * Inserts log row to database
+     * @param item
+     * @return
+     * @throws DatabaseException
+     */
     public LogRow insert(LogRow item) throws DatabaseException {
         SQLiteConnection db = new SQLiteConnection(new File(databaseLocation));
         try {
@@ -46,20 +57,40 @@ public class LogTable {
         return item;
     }
 
-    public Collection<LogRow> get() {
-        ArrayList<LogRow> mockList = new ArrayList<LogRow>();
-        for (int i = 0; i < 200; i++) {
-            LogRow row = new LogRow();
-            row.setMessage("Kaikki räjähti " + i);
-            row.setSeverity(LogSeverity.ERROR);
-            row.setSource("Barskaali");
-            Date d = new Date();
-            row.setTimestamp(d);
-            mockList.add(row);
-        }
-        return mockList;
-    }
+    /**
+     * Gets all log lines from database.
+     * @return All log lines from database.
+     * @throws DatabaseException Thrown if there is a problem with the database
+     */
+    public Collection<LogRow> get() throws DatabaseException {
+        SQLiteConnection db = new SQLiteConnection(new File(databaseLocation));
+        ArrayList<LogRow> logs = new ArrayList<LogRow>();
 
+        try {
+            db.open(false);
+            SQLiteStatement st = db.prepare(GETALL);
+            try {
+                while (st.step()) {
+                    LogRow row = new LogRow();
+                    row.setTimestamp(formatter.parse(st.columnString(0)));
+                    row.setMessage(st.columnString(2));
+                    row.setSource(st.columnString(3));
+                    row.setSeverity(LogSeverity.getSeverity(st.columnInt(1)));
+                    
+                    logs.add(row);
+                }
+            } catch (ParseException e) {
+                throw new DatabaseException("There was an invalid timestamp format in some column.", e);
+            } finally {
+                st.dispose();
+            }
+            db.dispose();
+        } catch (SQLiteException e) {
+            throw new DatabaseException("There was problem with database", e);
+        }
+        return logs;
+    }
+    
     /**
      * Creates insert statement for Playlist
      * @param PlayList playlist
