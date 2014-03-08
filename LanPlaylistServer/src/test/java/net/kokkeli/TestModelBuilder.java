05e698a05e5a43ab19abfd44b7cc196d518c2fea
Hekku2 <hekku2@gmail.com>
2014-03-08 19:40:08 +0200

    Created ModelBuilder to make automatic parsing of form parameters possible.

---
+++ LanPlaylistServer/src/test/java/net/kokkeli/TestModelBuilder.java
@@ -0,0 +1,78 @@
+package net.kokkeli;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import javax.ws.rs.core.MultivaluedMap;
+
+import net.kokkeli.data.Role;
+import net.kokkeli.resources.models.ModelPlaylistItem;
+import net.kokkeli.resources.models.ModelUser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestModelBuilder {
+    
+    @SuppressWarnings({ "unchecked", "static-method" })
+    @Test
+    public void testLongValueIsParsedCorrectly() throws InstantiationException, IllegalAccessException{
+        ModelBuilder<ModelPlaylistItem> builder = new ModelBuilder<ModelPlaylistItem>(ModelPlaylistItem.class);
+        
+        MultivaluedMap<String, String> map = mock(MultivaluedMap.class);
+        
+        when(map.containsKey("id")).thenReturn(true);
+        when(map.getFirst("id")).thenReturn("6  ");
+        
+        ModelPlaylistItem createdItem = builder.createModelFrom(map);
+        Assert.assertEquals(6, createdItem.getId());
+    }
+    
+    @SuppressWarnings({ "unchecked", "static-method" })
+    @Test
+    public void testStringValueIsUsedCorrectly() throws InstantiationException, IllegalAccessException{
+        ModelBuilder<ModelPlaylistItem> builder = new ModelBuilder<ModelPlaylistItem>(ModelPlaylistItem.class);
+        
+        MultivaluedMap<String, String> map = mock(MultivaluedMap.class);
+        
+        when(map.containsKey("artist")).thenReturn(true);
+        when(map.getFirst("artist")).thenReturn("artist");
+        
+        ModelPlaylistItem createdItem = builder.createModelFrom(map);
+        Assert.assertEquals("artist", createdItem.getArtist());
+    }
+    
+    @SuppressWarnings({ "unchecked", "static-method" })
+    @Test
+    public void testBooleanValueIsUsedCorrectly() throws InstantiationException, IllegalAccessException{
+        ModelBuilder<ModelPlaylistItem> builder = new ModelBuilder<ModelPlaylistItem>(ModelPlaylistItem.class);
+        
+        MultivaluedMap<String, String> map = mock(MultivaluedMap.class);
+        
+        when(map.containsKey("exists")).thenReturn(true);
+        when(map.getFirst("exists")).thenReturn("true");
+        
+        ModelPlaylistItem createdItem = builder.createModelFrom(map);
+        Assert.assertEquals(true, createdItem.getExists());
+        
+        when(map.containsKey("exists")).thenReturn(true);
+        when(map.getFirst("exists")).thenReturn("false");
+        
+        createdItem = builder.createModelFrom(map);
+        Assert.assertEquals(false, createdItem.getExists());
+    }
+    
+    @SuppressWarnings({ "unchecked", "static-method" })
+    @Test
+    public void testUserRoleValueIsParsedCorrectly() throws InstantiationException, IllegalAccessException{
+        ModelBuilder<ModelUser> builder = new ModelBuilder<ModelUser>(ModelUser.class);
+        
+        MultivaluedMap<String, String> map = mock(MultivaluedMap.class);
+        
+        when(map.containsKey("role")).thenReturn(true);
+        when(map.getFirst("role")).thenReturn("1");
+        
+        ModelUser createdItem = builder.createModelFrom(map);
+        Assert.assertEquals(Role.USER, createdItem.getRoleEnum());
+    }
+}
