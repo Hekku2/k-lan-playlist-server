7ad985b6203c41696b204232b1fe70b104ce1d73
Hekku2 <hekku2@gmail.com>
2014-12-20 20:11:14 +0200

    Fixed broken vlc playlist add test and added missing js-file

---
+++ LanPlaylistServer/src/main/resources/net/kokkeli/resources/js/playlist-vlc-add.js
@@ -0,0 +1,35 @@
+/**
+ * Functions related to VLC upload.
+ */
+
+$(document).ready(function(){
+	$('form').submit(function(event){
+		event.preventDefault();
+		//<input type="submit" value="Send" class="btn btn-primary" data-loading-text="Loading...">
+		$('input[type="submit"]').button('loading')
+		$.post("", $( "form" ).serialize()).fail(uploadfailed).done(uploadSuccess).always(resetButton);
+	});
+});
+
+function uploadfailed(event){
+	$.notify(event.responseText, { 
+		position:"top-center",
+		className: "error"
+	});
+}
+
+function uploadSuccess(event){
+	$.notify(event, { 
+		position:"top-center",
+		className: "success"
+	});
+	clearForm();
+}
+
+function resetButton(){
+	$('input[type="submit"]').button('reset')
+}
+
+function clearForm(){
+	$('form').find("input[type=text], textarea").val("");
+}
--- LanPlaylistServer/src/test/java/net/kokkeli/acceptance/pages/PagePlaylistAdd.java
+++ LanPlaylistServer/src/test/java/net/kokkeli/acceptance/pages/PagePlaylistAdd.java
@@ -5,6 +5,8 @@ import net.kokkeli.resources.models.ModelPlaylistItem;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PagePlaylistAdd extends BasePage{
 
@@ -21,6 +23,7 @@ public class PagePlaylistAdd extends BasePage{
         driver.findElement(By.name("track")).sendKeys(item.getTrack());
         driver.findElement(By.name("url")).sendKeys(item.getUrl());
         driver.findElement(By.cssSelector("input[type='submit']")).click();
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input:not([disabled])[type='submit']")));
     }
 
 }
