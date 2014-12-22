7e6d4385cb1cd29b62836ebcded3a8765c4c2bed
Hekku2 <hekku2@gmail.com>
2014-12-22 02:19:47 +0200

    Minor refactoriongs to log.js.

--- LanPlaylistServer/src/main/resources/net/kokkeli/resources/js/log.js
+++ LanPlaylistServer/src/main/resources/net/kokkeli/resources/js/log.js
@@ -2,19 +2,24 @@ var rowTemplate;
 
 $(document).ready(function(){
 	rowTemplate = $("#row-template").text();
-	updateRequests();
+	getLogRows();
 });
 
 /*
  * Updates requests table
  */
-function updateRequests(){
-	$.get('log/logs', function(data){
-		$("#log-table").html('');
-		
-		$.each(data, function(i, item){
-			item.timestamp = moment(item.timestamp).format('YYYY-MM-DD HH:mm:ss');
-			$("#log-table").append(nano(rowTemplate, item))
-		})
-	});
+function getLogRows(){
+	$.get('log/logs').done(updateTableRows);
+}
+
+function updateTableRows(data){
+	emptyRows();
+	$.each(data, function(i, item){
+		item.timestamp = moment(item.timestamp).format('YYYY-MM-DD HH:mm:ss');
+		$("#log-table").append(nano(rowTemplate, item))
+	})
+}
+
+function emptyRows(){
+	$("#log-table").html('');
 }
