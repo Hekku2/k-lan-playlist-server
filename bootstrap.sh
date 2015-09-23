b2ec09ab527a2b5d0d7dcd29a93a46084536599c
Hekku2 <hekku2@gmail.com>
2015-09-23 20:02:58 +0300

    1. Made some stuff silent 2. Cloned project 3. Installed project (npm install) 4. Started VLC 5. Started player service as PM2-process

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y git
+apt-get -qq update
+apt-get -qq install -y git
 
 #Node js
 curl --silent --location https://deb.nodesource.com/setup_4.x | bash -
-apt-get install --yes nodejs
+apt-get -qq install -y nodejs
+apt-get -qq install -y vlc pulseaudio
 
-apt-get install -y vlc pulseaudio
-
-mkdir lanplayer
-chown vagrant lanplayer
-
+#Install global npm stuff.
+npm config set loglevel warn
 npm install pm2 -g
 npm install gulp -g
+
+#Clone the base project (currently from branch)
+git clone https://github.com/Hekku2/k-lan-playlist-server.git -b Branch_nodejs
+chown -R vagrant k-lan-playlist-server
+
+#Install 
+(cd k-lan-playlist-server/PlayerService/ && npm install)
+
+#Start VLC as vagrant.
+su - vagrant -c 'vlc -d -I http --http-port 9999 --http-password test --sout '\''#standard{access=http,mux=ogg,dst=0.0.0.0:8081}'\'' --http-host=127.0.0.1 --sout-keep'
+
+#Start services
+su - vagrant -c 'pm2 start k-lan-playlist-server/PlayerService/src/player-service.js --watch'
+
+
+
