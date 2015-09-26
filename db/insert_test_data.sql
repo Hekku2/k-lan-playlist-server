4b97b4b847e9ff7d543f88fea615f1c4c8a5566e
Hekku2 <hekku2@gmail.com>
2015-09-26 16:20:23 +0300

    Fixed test data

--- db/insert_test_data.sql
+++ db/insert_test_data.sql
@@ -16,7 +16,6 @@ INSERT INTO tracks (Track, Artist, Location, Uploader) VALUES ('March enemy hell
 INSERT INTO tracks (Track, Artist, Location, Uploader) VALUES ('Jarmonet', 'Raimo Belsebup', 'X:\Music\song2.ogg',(SELECT Id FROM users WHERE UserName like 'user'));
 INSERT INTO tracks (Track, Artist, Location, Uploader) VALUES ('Mursujyrä', 'Marssimailat', 'X:\Music\Marssimailat - Mursujyrä.ogg',(SELECT Id FROM users WHERE UserName like 'admin'));
 INSERT INTO tracks (Track, Artist, Location, Uploader) VALUES ('Katujyrä', 'Marssimailat', 'X:\Music\Marssimailat - Katujyrä.ogg',(SELECT Id FROM users WHERE UserName like 'admin'));
-INSERT INTO tracks (Track, Artist, Location, Uploader) VALUES ('Katujyrä', 'Marssimailat', 'X:\Music\Marssimailat - Katujyrä.ogg', null);
 
 -- Playlists
 INSERT INTO playlists (Name) VALUES ('Test playlist 2014');
@@ -43,6 +42,7 @@ VALUES (
     (SELECT Id FROM tracks WHERE Track like 'Mursujyrä' and Artist like 'Marssimailat'),
     (SELECT Id FROM playlists WHERE Name like 'Test playlist 2014')
 );
+
 INSERT INTO tracks_playlists (TrackId, PlayListId)
 VALUES (
     (SELECT Id FROM tracks WHERE Track like 'Katujyrä' and Artist like 'Marssimailat'),
@@ -54,6 +54,7 @@ VALUES (
 	'http://www.example.com/',	'',	'Marssimailat - Mursujyrä.ogg',	datetime('now'),0,
 	(SELECT Id FROM tracks WHERE Track like 'Mursujyrä' and Artist like 'Marssimailat')
 );
+
 INSERT INTO fetch_requests(Location, Handler, DestinationFile, LastUpdated, FetchStatus, Track)
 VALUES (
 	'http://www.example.com/',	'',	'Marssimailat - Katujyrä.ogg',	datetime('now'),3,
