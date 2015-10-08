9eed77d383a07e7fc912ce1df8bdd37df9907abd
Hekku2 <hekku2@gmail.com>
2015-10-08 20:35:48 +0300

    Fixed test data initialization to include user creation.

--- UserService/test/test-db-tools.js
+++ UserService/test/test-db-tools.js
@@ -1,14 +1,24 @@
 var config = require('config');
-var cp = require("child_process");
+var cp = require('child_process');
 
 exports.initializeTestData = function (){
     var user = '--user=' + config.get('TestSettings.admin.user');
     var password = '--password=' + config.get('TestSettings.admin.password');
     var database = config.get('Database.database');
-    var cmdLine = 'mysql '+ user + ' ' + password + ' ' + database+ ' < ../db/insert_test_data.sql';
-    console.log(cmdLine);
-    cp.exec(cmdLine, function(error,stdout,stderr) {
-        console.log(error,stdout,stderr);
+    var users = 'mysql '+ user + ' ' + password + ' ' + database+ ' < ../db/initialize_users.sql';
+    console.log(users);
+    cp.exec(users, function(error,stdout,stderr) {
+        if (error){
+            console.log(error);
+            return;
+        }
+
+        var testData = 'mysql '+ user + ' ' + password + ' ' + database+ ' < ../db/insert_test_data.sql';
+        cp.exec(testData, function(error,stdout,stderr) {
+            if (error){
+                console.log(error);
+            }
+        });
     });
 };
 
