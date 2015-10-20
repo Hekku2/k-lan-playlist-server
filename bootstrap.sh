eba02cd3da9060936bfdc20c8ef24204aa4929fa
Hekku2 <hekku2@gmail.com>
2015-10-20 22:24:32 +0300

    Moved part of vagrant initialization to user-install.sh. These are run as regular vagrant user instead of root. Also added more memory to virtual box.

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,6 +20,8 @@ npm install gulp -g
 npm install mocha -g
 npm install jenkins-mocha -g 
 npm install bower -g
+npm install phantomjs -g
+
 
 #Youtube-dl
 curl --silent https://yt-dl.org/latest/youtube-dl -o /usr/local/bin/youtube-dl
@@ -27,27 +29,3 @@ chmod a+rx /usr/local/bin/youtube-dl
 add-apt-repository -y ppa:mc3man/trusty-media
 apt-get -qq update
 apt-get -qq install -y ffmpeg
-
-
-#Clone the base project (currently from branch)
-git clone https://github.com/Hekku2/k-lan-playlist-server.git -b Branch_nodejs
-chown -R vagrant k-lan-playlist-server
-
-#Install 
-(cd k-lan-playlist-server/PlayerService/ && npm install)
-(cd k-lan-playlist-server/UserService/ && npm install)
-(cd k-lan-playlist-server/LanPlayerClientSide/ && npm install)
-
-#Start VLC as vagrant.
-su - vagrant -c 'vlc -d -I http --http-port 9999 --http-password test --sout '\''#standard{access=http,mux=ogg,dst=0.0.0.0:9090}'\'' --http-host=127.0.0.1 --sout-keep'
-
-#Start services
-su - vagrant -c 'cd k-lan-playlist-server/PlayerService/ && pm2 -s start src/start.js --watch --name player-service'
-su - vagrant -c 'cd k-lan-playlist-server/UserService/ && pm2 -s start src/start.js --watch --name user-service'
-su - vagrant -c 'cd k-lan-playlist-server/LanPlayerClientSide/ && pm2 -s start src/start.js --watch --name client-side'
-su - vagrant -c 'pm2 -s save'
-
-#Initialiaze database for dev deployment
-mysql --user=root --password=root < k-lan-playlist-server/db/database_initialization.sql
-mysql --user=root --password=root < k-lan-playlist-server/db/insert_test_data.sql
-
