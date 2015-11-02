d9ccdc09373b0be5d0c49226abbcc0e40ee72d3f
Hekku2 <hekku2@gmail.com>
2015-11-02 21:30:08 +0200

    Added bootstrap

--- LanPlayerClientSide/src/routes.js
+++ LanPlayerClientSide/src/routes.js
@@ -3,7 +3,11 @@ var express = require('express');
 exports.setup = function setup(app, handlers) {
     app.use('/js/angular/', express.static('bower_components/angular/'));
     app.use('/js/angular-route/', express.static('bower_components/angular-route/'));
+    app.use('/js/bootstrap/', express.static('bower_components/bootstrap/dist/js/'));
+    app.use('/js/jquery/', express.static('bower_components/jquery/dist/'));
     app.use('/js/', express.static('src/public/js/'));
+
+    app.use('/css/bootstrap/', express.static('bower_components/bootstrap/dist/css/'));
 
     app.get('/users', handlers.users.users);
     app.get('/user/:userId', handlers.users.user);
