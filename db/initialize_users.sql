8485f5d82c30ed6487dc901529c01e2721291862
Hekku2 <hekku2@gmail.com>
2015-11-04 22:11:37 +0200

    Testing different user creation type.

--- db/initialize_users.sql
+++ db/initialize_users.sql
@@ -1,4 +1,5 @@
 -- This script creates database user. In production environment, different password should be used.
-GRANT ALL ON `database`.* TO 'user-service'@'localhost' IDENTIFIED BY 'test';
-GRANT USAGE ON *.* TO 'user-service'@'localhost';
-GRANT SELECT, UPDATE, INSERT  ON TABLE `lanplayer`.`users` TO 'user-service'@'localhost';
+DROP USER 'user-service'@'localhost';
+CREATE USER 'user-service'@'localhost' IDENTIFIED BY 'test';
+GRANT USAGE ON .* TO 'user-service'@'localhost';
+GRANT SELECT, UPDATE, INSERT ON TABLE `lanplayer`.`users` TO 'user-service'@'localhost';
