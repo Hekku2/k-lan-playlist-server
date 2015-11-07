ed5679a93212d2e64fe65eeee5e03f3a513b187c
Hekku2 <hekku2@gmail.com>
2015-11-07 20:31:00 +0200

    Hopefully fixing user initiliaization for jenkins and vagrant.

--- UserService/test/test-db-tools.js
+++ UserService/test/test-db-tools.js
@@ -2,35 +2,26 @@ var config = require('config');
 var cp = require('child_process');
 
 exports.initializeTestData = function (){
-    var user = config.get('TestSettings.admin.user');
-    var userParameter;
-    if (user)
-        userParameter = '--user=' + user;
-    else
-        userParameter = '';
+    var parameterOrEmpty = function(parameter, configuration){
+        var value = config.get(configuration);
+        if (value)
+            return '--' + parameter + '=' + value;
+        else
+            return '';
+    };
 
-    var passwordParameter;
-    var password = config.get('TestSettings.admin.password');
-    if (password)
-        passwordParameter = '--password=' + password;
-    else
-        passwordParameter = '';
+    var userParameter = parameterOrEmpty('user', 'TestSettings.admin.user');
+    var passwordParameter = parameterOrEmpty('password', 'TestSettings.admin.password');
 
     var database = config.get('Database.database');
-    var users = 'mysql '+ userParameter + ' ' + passwordParameter + ' ' + database+ ' < ../db/initialize_users.sql';
-    console.log(users);
-    cp.exec(users, function(error,stdout,stderr) {
+    var initializeUsers = config.get('TestSettings.initialize-users');
+
+    var userInitializationParameter = initializeUsers ? ' < ../db/initialize_users.sql': '';
+
+    var command = 'mysql '+ userParameter + ' ' + passwordParameter + ' ' + database + userInitializationParameter + ' < ../db/insert_test_data.sql';
+    cp.exec(command, function(error,stdout,stderr) {
         if (error){
             console.log(error);
-            return;
         }
-
-        var testData = 'mysql '+ userParameter + ' ' + passwordParameter + ' ' + database+ ' < ../db/insert_test_data.sql';
-        cp.exec(testData, function(error,stdout,stderr) {
-            if (error){
-                console.log(error);
-            }
-        });
     });
 };
-
--- db/initialize_users.sql
+++ db/initialize_users.sql
@@ -1,5 +1,4 @@
 -- This script creates database user. In production environment, different password should be used.
-DROP USER 'user-service'@'localhost';
-CREATE USER 'user-service'@'localhost' IDENTIFIED BY 'test';
+GRANT USAGE ON `database`.* to 'user-service'@'localhost' identified by 'test';
 GRANT USAGE ON *.* TO 'user-service'@'localhost';
 GRANT SELECT, UPDATE, INSERT  ON TABLE `lanplayer`.`users` TO 'user-service'@'localhost';
