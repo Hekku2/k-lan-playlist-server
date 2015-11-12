3d7e19ddf130b6252a52b6cc123afd56497cff24
Hekku2 <hekku2@gmail.com>
2015-11-12 21:46:10 +0200

    Removing usersservice (moving to to submodule)

--- UserService/src/handlers/status-handler.js
+++
@@ -1,8 +0,0 @@
-var packageJson = require('../../package.json');
-
-exports.status = function status(req, res) {
-    res.send({
-        status: 'OK',
-        version: packageJson.version
-    });
-};
--- UserService/src/routes.js
+++
@@ -1,6 +0,0 @@
-exports.setup = function setup(app, handlers) {
-    app.get('/api/status', handlers.status.status);
-    app.get('/api/users', handlers.user.list);
-    app.get('/api/user/:id', handlers.user.single);
-    app.post('/api/user/:id', handlers.user.update);
-};
