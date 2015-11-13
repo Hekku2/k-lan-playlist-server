d8e1d0fff57b3365f7ae0bde4bc5800a97f6a5cf
Hekku2 <hekku2@gmail.com>
2015-11-13 17:00:48 +0200

    Removed LanPlayerClientSide for coming submodule.

--- LanPlayerClientSide/src/public/js/angularConfig.js
+++
@@ -1,6 +0,0 @@
-angular.module('clientSideLanPlayer.config', []).constant('appConfig', {
-    'services': {
-        'PlayerService': 'http://localhost:8080/api/',
-        'UserService': 'http://localhost:8081/api/'
-    }
-});
--- LanPlayerClientSide/src/public/js/controllers/mainControllers.js
+++
@@ -1,4 +0,0 @@
-var statusControllers = angular.module('mainControllers', []);
-
-statusControllers.controller("MainCtrl", function($scope) {
-});
